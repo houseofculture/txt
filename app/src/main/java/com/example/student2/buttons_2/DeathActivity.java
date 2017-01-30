8c0be8e6deb44dac6996c9639e1ddad7f4c8c637
houseofculture <mortypines@gmail.com>
2017-01-30 18:53:15 +0300

    sss

--- app/src/main/java/com/example/student2/buttons_2/DeathActivity.java
+++ app/src/main/java/com/example/student2/buttons_2/DeathActivity.java
@@ -1,7 +1,9 @@
 package com.example.student2.buttons_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class DeathActivity extends AppCompatActivity {
@@ -12,5 +14,9 @@ public class DeathActivity extends AppCompatActivity {
         setContentView(R.layout.activity_death);
         TextView tv = (TextView)findViewById(R.id.tw);
         tv.setText(tv.getText()+"\n"+"Ваши результаты:\nHP:"+MainActivity.player.hp+"\nDMG:"+MainActivity.player.dmg);
+    }
+
+    public void deathClick(View view) {
+        Intent i = new Intent(this, startActivity.class);
     }
 }
